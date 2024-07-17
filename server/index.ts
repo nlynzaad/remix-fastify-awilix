a4c39b87698c67bdf1178c464b82eb368cff4219
Nico Lynzaad <nlynzaad@zylangroup.com>
2024-07-18 00:25:37 +0200

    detail http/2 & https usage if required.

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,35 @@
+/*
+import * as fs from "node:fs";
+import * as path from "node:path";
+ */
+
 import process from "node:process";
-import { remixFastify } from "@mcansh/remix-fastify";
-import { installGlobals } from "@remix-run/node";
-import { fastify } from "fastify";
+import {installGlobals} from "@remix-run/node";
+import {fastify} from "fastify";
+import {remixFastify} from "@mcansh/remix-fastify";
 import {fastifyAwilixPlugin} from "@fastify/awilix";
 import {container} from "./di";
 
 installGlobals();
 
-const app = fastify({ logger: true });
+const app = fastify({logger: true});
+
+/*
+To run app with http 2. create a key and cert and place it in the ssl folder.
+This can be helpful if you need it during development for example when developing file uploads that uses Request Body streams: https://developer.chrome.com/docs/capabilities/web-apis/fetch-streaming-requests#doesnt_work_on_http1x
+In production your app will probably be hosted behind a proxy or hosted elsewhere, where https/http2 is provided external to the application.
+*/
+
+/*
+const app = fastify({
+	logger: true,
+	http2: true,
+	https: {
+		key: fs.readFileSync(path.join(process.cwd(), 'ssl', 'server.key')),
+		cert: fs.readFileSync(path.join(process.cwd(), 'ssl', 'server.crt')),
+	}
+});
+*/
 
 await app.register(fastifyAwilixPlugin, {
 	disposeOnClose: true,
@@ -18,12 +40,12 @@ await app.register(fastifyAwilixPlugin, {
 
 await app.register(remixFastify, {
 	getLoadContext(request) {
-		return { di: request.diScope.cradle};
+		return {di: request.diScope.cradle};
 	}
 });
 
 const port = Number(process.env.PORT) || 3000;
 const host = process.env.HOST === "true" ? "0.0.0.0" : "127.0.0.1";
 
-let address = await app.listen({ port, host });
+let address = await app.listen({port, host});
 console.log(`✅ app ready: ${address}`);
